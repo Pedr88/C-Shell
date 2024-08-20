13a34c66877d8b0dd53fc63a611ed8643fc8be28
Pedr88 <teixeirapedro482@gmail.com>
2024-08-20 14:22:08 -0300

    Add "exit" command

--- app/main.c
+++ app/main.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
 
 int main() {
-  bool running = true;
-  
   char input[100];
-  while (running == true) {
+  char exit[4] = "exit";
+
+  while (1) {
     printf("$ ");
     fflush(stdout);
 
+    // Gets user input
     fgets(input, 100, stdin);
-
     input[strcspn(input, "\n")] = 0;
-    printf("%s: command not found\n", input);
+
+    if (strcmp(input, "exit") == 0) {
+      printf("Exiting shell...\a\n");
+      break;
+    } else {
+      printf("%s: command not found\n", input);
+    } 
   } 
 
   return 0;
